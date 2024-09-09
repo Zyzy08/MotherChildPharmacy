f425ea51aacd9e6ff7287db085aa2282f5a82d70
LanceTiangco <152777574+LanceTiangco@users.noreply.github.com>
2024-09-09 23:30:07 +0800

    Current database upload (test)

---
+++ motherchildpharmacy.sql
@@ -0,0 +1,317 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 09, 2024 at 05:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `motherchildpharmacy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `AccountID` int(10) UNSIGNED NOT NULL,
+  `employeeName` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `accountName` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `picture` text NOT NULL DEFAULT '../resources/profile_icon.png',
+  `dateCreated` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` varchar(255) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`AccountID`, `employeeName`, `role`, `accountName`, `password`, `picture`, `dateCreated`, `status`) VALUES
+(3, 'Lance Tiangco', 'Admin', 'ltiangco', '123', 'uploads/dubu2.jpg', '2024-09-01 23:42:57', 'Active'),
+(19, 'Aileen Castro', 'Pharmacy Assistant', 'adminAC', 'yehey', 'uploads/pitied.png', '2024-09-06 21:39:08', 'Active'),
+(20, 'Two Pangalawa', 'Purchaser / Pharmacy Assistant', '2P', 'eztwo', '../resources/profile_icon.png', '2024-09-06 21:39:37', 'Inactive'),
+(21, 'Chou Tzuyu', 'PA', 'Tzuyu', 'runaway', 'uploads/tzu.jpg', '2024-09-06 21:40:07', 'Active'),
+(22, 'Wise Wolf Holo', 'PurchaserPA', 'Hololive', 'hehelolo', 'uploads/Holo.jpg', '2024-09-06 21:40:30', 'Active'),
+(24, 'Kim Dahyun', 'Pharmacy Assistant', 'kimdahyun', 'bibimbap', 'uploads/dubu.jpg', '2024-09-06 21:42:20', 'Active'),
+(28, 'Apple', 'Admin', 'apldap', 'apoldeez', 'uploads/half apple.png', '2024-09-06 22:18:41', 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goodsissue`
+--
+
+CREATE TABLE `goodsissue` (
+  `IssueID` int(11) NOT NULL,
+  `ItemID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Reason` text NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goodsreturn`
+--
+
+CREATE TABLE `goodsreturn` (
+  `goodsReturnID` int(11) NOT NULL,
+  `originalInvoiceID` int(11) NOT NULL,
+  `newInvoiceID` int(11) NOT NULL,
+  `returnDetails` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`returnDetails`))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `ItemID` int(11) NOT NULL,
+  `ItemName` varchar(255) NOT NULL,
+  `GenericName` varchar(255) DEFAULT NULL,
+  `BrandName` varchar(255) DEFAULT NULL,
+  `ItemType` varchar(50) DEFAULT NULL,
+  `Mass` varchar(50) DEFAULT NULL,
+  `UnitOfMeasure` varchar(20) DEFAULT NULL,
+  `InStock` int(10) DEFAULT NULL,
+  `Ordered` int(10) NOT NULL,
+  `ReorderLevel` int(11) DEFAULT NULL,
+  `PricePerUnit` decimal(10,2) DEFAULT NULL,
+  `SupplierID` int(11) DEFAULT NULL,
+  `Notes` text DEFAULT NULL,
+  `Status` enum('Active','Inactive') NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `productlot`
+--
+
+CREATE TABLE `productlot` (
+  `LotNumber` int(11) NOT NULL,
+  `ItemID` int(11) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `ExpirationDate` date NOT NULL,
+  `isExpired` enum('1','0') NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `purchaseorders`
+--
+
+CREATE TABLE `purchaseorders` (
+  `PurchaseOrderID` int(11) NOT NULL,
+  `OrderDate` datetime DEFAULT current_timestamp(),
+  `SupplierID` int(11) NOT NULL,
+  `AccountID` int(10) UNSIGNED NOT NULL,
+  `ItemName` text DEFAULT NULL,
+  `ItemQuantity` int(11) DEFAULT NULL,
+  `TotalCost` decimal(10,2) DEFAULT NULL,
+  `Status` enum('Pending','Received','Cancelled') DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `InvoiceID` int(11) NOT NULL,
+  `SaleDate` datetime DEFAULT current_timestamp(),
+  `AccountID` int(10) UNSIGNED DEFAULT NULL,
+  `SalesDetails` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`SalesDetails`)),
+  `TotalItems` int(11) NOT NULL,
+  `Subtotal` decimal(10,2) DEFAULT 0.00,
+  `Tax` decimal(10,2) DEFAULT 0.00,
+  `Discount` decimal(10,2) DEFAULT 0.00,
+  `NetAmount` decimal(10,2) GENERATED ALWAYS AS (`Subtotal` - `Discount` + `Tax`) STORED,
+  `AmountPaid` decimal(10,2) DEFAULT 0.00,
+  `AmountChange` decimal(10,2) GENERATED ALWAYS AS (`AmountPaid` - `NetAmount`) STORED,
+  `PaymentMethod` enum('Cash','GCash') NOT NULL,
+  `Status` enum('Sales','Return','Return/Exchange') NOT NULL,
+  `RefundAmount` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `SupplierID` int(11) NOT NULL,
+  `SupplierName` varchar(255) NOT NULL,
+  `AgentName` varchar(255) DEFAULT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Status` enum('Active','Inactive') DEFAULT 'Active',
+  `Notes` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`AccountID`),
+  ADD UNIQUE KEY `accountName` (`accountName`);
+
+--
+-- Indexes for table `goodsissue`
+--
+ALTER TABLE `goodsissue`
+  ADD PRIMARY KEY (`IssueID`),
+  ADD KEY `GI_ForeignKey_ItemID` (`ItemID`);
+
+--
+-- Indexes for table `goodsreturn`
+--
+ALTER TABLE `goodsreturn`
+  ADD KEY `GR_ForeignKey_InvoiceID` (`originalInvoiceID`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`ItemID`),
+  ADD KEY `ForeignKey_SupplierID` (`SupplierID`);
+
+--
+-- Indexes for table `productlot`
+--
+ALTER TABLE `productlot`
+  ADD PRIMARY KEY (`LotNumber`),
+  ADD KEY `PL_ForeignKey_ItemID` (`ItemID`);
+
+--
+-- Indexes for table `purchaseorders`
+--
+ALTER TABLE `purchaseorders`
+  ADD PRIMARY KEY (`PurchaseOrderID`),
+  ADD KEY `PO_ForeignKey_AccountID` (`AccountID`),
+  ADD KEY `PO_ForeignKey_SupplierID` (`SupplierID`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`InvoiceID`),
+  ADD KEY `ForeignKey_AccountID` (`AccountID`);
+
+--
+-- Indexes for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`SupplierID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `AccountID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `goodsissue`
+--
+ALTER TABLE `goodsissue`
+  MODIFY `IssueID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `ItemID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `purchaseorders`
+--
+ALTER TABLE `purchaseorders`
+  MODIFY `PurchaseOrderID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `InvoiceID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `SupplierID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `goodsissue`
+--
+ALTER TABLE `goodsissue`
+  ADD CONSTRAINT `GI_ForeignKey_ItemID` FOREIGN KEY (`ItemID`) REFERENCES `inventory` (`ItemID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `goodsreturn`
+--
+ALTER TABLE `goodsreturn`
+  ADD CONSTRAINT `GR_ForeignKey_InvoiceID` FOREIGN KEY (`originalInvoiceID`) REFERENCES `sales` (`InvoiceID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `ForeignKey_SupplierID` FOREIGN KEY (`SupplierID`) REFERENCES `suppliers` (`SupplierID`) ON DELETE SET NULL ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `productlot`
+--
+ALTER TABLE `productlot`
+  ADD CONSTRAINT `PL_ForeignKey_ItemID` FOREIGN KEY (`ItemID`) REFERENCES `inventory` (`ItemID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `purchaseorders`
+--
+ALTER TABLE `purchaseorders`
+  ADD CONSTRAINT `PO_ForeignKey_AccountID` FOREIGN KEY (`AccountID`) REFERENCES `accounts` (`AccountID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `PO_ForeignKey_SupplierID` FOREIGN KEY (`SupplierID`) REFERENCES `suppliers` (`SupplierID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `ForeignKey_AccountID` FOREIGN KEY (`AccountID`) REFERENCES `accounts` (`AccountID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
