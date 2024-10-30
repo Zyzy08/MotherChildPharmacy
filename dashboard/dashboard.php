56b85baff4b2d2330c4949a58fe6172db588ae66
Sayra Jackson <jacksonsayra2@gmail.com>
2024-10-30 11:16:16 +0800

    Sayra - Updated 10-30-2024

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -227,6 +227,18 @@
             <!-- Inventory Card -->
             <div class="col-xxl-4 col-md-6">
               <div class="card info-card sales-card">
+                <div class="filter">
+                  <a id="filter-inv" class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
+                  <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+                    <li class="dropdown-header text-start">
+                      <h6>Filter</h6>
+                    </li>
+                    <li><a class="dropdown-item" href="#" data-period="today">In-Stock</a></li>
+                    <li><a class="dropdown-item" href="#" data-period="month">Low-Stock</a></li>
+                    <li><a class="dropdown-item" href="#" data-period="year">Out-of-Stock</a></li>
+                  </ul>
+                </div>
+
                 <div class="card-body">
                   <h5 class="card-title">Inventory</h5>
                   <div class="d-flex align-items-center">
@@ -714,7 +726,7 @@
 
   <!-- Template Main JS File -->
   <script src="../main.js"></script>
-  <script src="dash.js"></script>
+  <script src="dash copy.js"></script>
 
 </body>
 
