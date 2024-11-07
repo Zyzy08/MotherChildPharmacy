f6a1fadff81e86882f5b1c31d2b7523c2d3a0a8d
Sayra Jackson <jacksonsayra2@gmail.com>
2024-11-08 06:32:43 +0800

    Sayra - Updated 11-8-2024

---
+++ pos/fetchProductData copy.php
@@ -0,0 +1,46 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "motherchildpharmacy";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Get the search query
+$searchQuery = isset($_POST['query']) ? $_POST['query'] : '';
+
+// Prepare the SQL statement to fetch products
+$sql = "SELECT ItemID, BrandName, GenericName, Mass, UnitOfMeasure, InStock, PricePerUnit, ProductIcon, ProductCode, ReorderLevel, Status 
+        FROM inventory";
+
+if ($searchQuery) {
+    $sql .= " WHERE (BrandName LIKE ? OR GenericName LIKE ? OR ProductCode LIKE ?)";
+    $stmt = $conn->prepare($sql);
+    $searchTerm = "%" . $searchQuery . "%";
+    $stmt->bind_param('sss', $searchTerm, $searchTerm, $searchTerm);
+    $stmt->execute();
+    $result = $stmt->get_result();
+} else {
+    $result = $conn->query($sql);
+}
+
+$products = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $products[] = $row;
+    }
+}
+
+// Output the products as a comment
+echo '<!-- ' . json_encode($products) . ' -->';
+
+// Close the connection
+$conn->close();
+?>
